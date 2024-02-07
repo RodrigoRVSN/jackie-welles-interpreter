f97fb1631f8dd3ef373a58adfe474867fb71caa7
rodrigorvsn <rodrigovictor81@gmail.com>
2024-02-06 23:10:49 -0300

    test: add initial return statement test cases

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -41,6 +41,34 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestReturnStatement(t *testing.T) {
+	input := `
+    return 5;
+    return 10;
+    return 67303;
+  `
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements, got=%d", len(program.Statements))
+	}
+
+	for _, statement := range program.Statements {
+		returnStatement, ok := statement.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("statement not *ast.ReturnStatement, got=%T", returnStatement)
+			continue
+		}
+		if returnStatement.TokenLiteral() != "return" {
+			t.Errorf("returnStatement.TokenLiteral not 'return', got %q", returnStatement.TokenLiteral())
+		}
+	}
+}
+
 func testLetStatement(t *testing.T, statement ast.Statement, name string) bool {
 	if statement.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral() not 'let', got=%q", statement.TokenLiteral())
